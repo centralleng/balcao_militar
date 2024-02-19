91bf667217ff260c7a045fde1cea63098421fe1e
TechFontes <tec.fontes1@gmail.com>
2024-02-19 17:23:04 -0300

    acerto

--- src/server.ts
+++ src/server.ts
@@ -41,11 +41,11 @@ app.listen(port, function () {
     console.log(`O servidor está rodando da porta ${port}...`);
 });
 
-// Bot_bd_mil.execute()
+Bot_bd_mil.execute()
 const bot_venda = new Bot_bd_mil_venda
 bot_venda.execute()
-// Bot_bd_mil_comprar.execute()
-// Bot_bd_mil_alertas.execute()
+Bot_bd_mil_comprar.execute()
+Bot_bd_mil_alertas.execute()
 
 // Bot_bd_mil_news.execute()
 // Bot_bd_mil_suporte.execute()
